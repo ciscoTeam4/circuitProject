d26ea58e932a2feaa87966d7a663fd6468aea20e
GrytsakV <grytsak.v@gmail.com>
2017-06-13 20:03:52 -0400

    Add files via upload

---
+++ circuitProject-master2/js/moduleLogin.js
@@ -0,0 +1,17 @@
+<!-- script pour module login -->
+			$(document).ready(function(){
+				$("#myBtn1").click(function(){
+					$("#login-modal").modal();
+				});
+			});
+
+			
+			
+			
+			
+			
+		$(document).ready(function(){
+    $(".col-sm-4.myBtn").click(function(){
+        $("#myModal").modal();
+    });
+});
