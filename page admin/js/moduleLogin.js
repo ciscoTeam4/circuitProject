c86631c26218d9f7cb49c67c494fd74874646e0d
mino9789 <mino9789@gmail.com>
2017-06-14 03:50:30 -0400

    Add files via upload

---
+++ page admin/js/moduleLogin.js
@@ -0,0 +1,20 @@
+// script pour module logout
+			$(document).ready(function(){
+				$("#myBtn1").click(function(){
+					$("#login-modal").modal();
+				});
+			});
+// script pour module ajouter un circuit
+				$(document).ready(function(){
+				$("#myBtn2").click(function(){
+					$("#AjouterCircuit-modal").modal();
+				});
+			});
+			
+			
+			
+		$(document).ready(function(){
+    $(".col-sm-4.myBtn").click(function(){
+        $("#myModal").modal();
+    });
+});
